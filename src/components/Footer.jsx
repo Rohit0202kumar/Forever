c2c10a9f0c663de976cac0fd4408ac127cd70432
Rohit0202kumar <rohitrajbhar0202@gmail.com>
2024-09-18 23:50:15 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className=''>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             <div>
-                <img src={assets.logo3} className='mb-5 w-32'/>
+                <img src={assets.logo} className='mb-5 w-32'/>
                 <p className='w-full md:w-2/3 text-gray-600'>Discover a world of convenience with our eCommerce website, offering diverse products, seamless shopping, and exceptional service for a delightful experience.</p>
             </div>
             <div>
